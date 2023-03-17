e647cb4faf99cef6fc65f34f32bb48e860eea377
Jaskiran Singh <jsingh8986@yahoo.com>
2023-03-17 14:55:19 -0700

    validations complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
     has_many :comments, through: :posts
     has_many :followees, foreign_key: :followee_id, class_name: "Follower"
     has_many :followers, through: :followees
+
+    validates :username, :password, :name, presence: true
+    validates :username, uniqueness: true
+    validates :password, length: { minimum: 8, maximum: 20 }
+    validates :bio, length: { maximum: 300 }
 end
