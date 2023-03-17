f3bebbe2f8a48e1a4be23337a4b0f96cafb6c4a9
Jaskiran Singh <jsingh8986@yahoo.com>
2023-03-17 12:21:42 -0700

    associations complete, self-join complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :posts
+    has_many :comments, through: :posts
+    has_many :followees, foreign_key: :followee_id, class_name: "Follower"
+    has_many :followers, through: :followees
 end
